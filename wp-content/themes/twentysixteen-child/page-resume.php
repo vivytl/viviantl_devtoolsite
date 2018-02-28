6be261f606b492c10a4012b8aaef85eef245cb58
vivytl <vivitliu@hotmail.com>
2018-02-28 14:57:21 -0800

    added some pages to child themes

--- wp-content/themes/twentysixteen-child/page-resume.php
+++ wp-content/themes/twentysixteen-child/page-resume.php
@@ -1,30 +1,8 @@
 <?php
-/**
- * The template for displaying pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that
- * other "pages" on your WordPress site will use a different template.
- *
- * @package WordPress
- * @subpackage Twenty_Sixteen
- * @since Twenty Sixteen 1.0
- */
-
-
-/*if (is_front_page()) :
-	get_header('home')
-elseif ( is_page('About')):
-get_header ('about');
-else:
-get_header();
-endif;?> */
+Template Name: [my-template].php
 
 
 
-
-
-get_header(); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
@@ -54,3 +32,4 @@ get_header(); ?>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
+?>
